07ae01543d33376c470513abe78a9f8d89f94ec5
thoratpratik2323-hue <thoratpratik2323@gmail.com>
2026-07-08 23:49:18 +0530

    chore: add push_to_github.bat helper script

---
+++ push_to_github.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ==============================================
+echo  IP Prime OS - Git Push to IP-Verse-Mafia
+echo ==============================================
+echo.
+echo Pushing latest commits to:
+echo https://github.com/thoratpratik2323-hue/IP-Verse-Mafia.git
+echo.
+git push origin main
+echo.
+echo ==============================================
+echo  Done! Press any key to exit.
+echo ==============================================
+pause
